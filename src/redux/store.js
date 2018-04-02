e0994c273201db9f9e69ad5561bd77be458513c7
Matthew Steel <matthewsteel@repsilat.com>
2018-04-02 12:38:18 -0700

    Take name out of ref in default state

--- src/redux/store.js
+++ src/redux/store.js
@@ -52,7 +52,7 @@ const initialState = {
     id: 'cell3',
     tableId: 'table0',
     name: 'Tui',
-    formula: [{ ref: 'cell2', name: 'Wiremu' }, { op: '+' }, { value: 'quux' }],
+    formula: [{ ref: 'cell2' }, { op: '+' }, { value: 'quux' }],
     x: 1,
     y: 1,
     width: 3,
