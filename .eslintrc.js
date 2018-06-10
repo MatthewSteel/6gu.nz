af15099c938ab440ff98999e1d03ede6590d60a3
Matthew Steel <matthewsteel@repsilat.com>
2018-06-11 10:30:56 +1200

    Move client one dir down

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'import/extensions': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'no-bitwise': 0,
+    'no-continue': 0,
+    'no-use-before-define': 0,
+    'no-eval': 0,
+    'no-restricted-syntax': 0,
+    'no-mixed-operators': 0,
+    'no-plusplus': 0,
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'object-curly-newline': [
+      'error',
+      {
+        consistent: true,
+      },
+    ],
+  },
+  env: {
+    browser: true,
+    jasmine: true,
+  },
+};
