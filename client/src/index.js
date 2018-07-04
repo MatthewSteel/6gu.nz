00cfd9363c00de1dee5def3efbf564645847cf6a
STEEL <matthewsteel@repsilat.com>
2018-07-05 11:12:54 +1200

    Do not use service workers
    Annoying: They do not let us serve dynamic content from
    the webserver. We might be able to put that stuff on a
    different port or something, but screw it this all seems
    like a dumb idea.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
